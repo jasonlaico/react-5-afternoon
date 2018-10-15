937aaa01d584a1af2972b86705c21eaaca7ea0c4
Jason Laico <jason.laico@gmail.com>
2018-10-15 17:38:01 -0500

    done

--- src/components/WizardSix/WizardSix.js
+++ src/components/WizardSix/WizardSix.js
@@ -1,28 +1,49 @@
-import React,  { Component } from 'react';
-import { Link } from 'react-router-dom';
-
-class WizardSix extends Component {
-
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">                    <p>What is the estimated purchase price?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateCost}/> <br />
-
-
-                    <p>How much are you putting down as a down payment?</p> <br />
-                        
-                        
-                        <input type="text" placeholder="amount" onChange={this.props.updateDownPayment}/>                    
-                        
-                    
-                    <Link to="/wSeven"><button className="margin-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
-}
-
-export default WizardSix;
+const initialState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: false,
+    realEstateAgent: "false",
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: 'aa',
+    lastName: '',
+    email: ''
+ }
+ 
+ const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+ const UPDATE_PROPERTY_TYPE = 'UPDATE_PROPERTY_TYPE'; 
+ 
+ function reducer( state = initialState, action ){ 
+     switch( action.type ){
+       case UPDATE_LOAN_TYPE:
+           return Object.assign( {}, state, { loanType: action.payload } );
+ 
+       case UPDATE_PROPERTY_TYPE:
+           return Object.assign( {}, state, { propertyType: action.payload } );
+ 
+       default: return state;
+     }
+ } 
+ 
+ export function updateLoanType( loanType ){
+   return {
+     type: UPDATE_LOAN_TYPE,
+     payload: loanType
+   }
+ }
+ 
+ export function updatePropertyType( property ) {
+   return {
+     type: UPDATE_PROPERTY_TYPE,
+     payload: property
+   }
+ }
+ 
+ export default reducer; 
